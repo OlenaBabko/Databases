5c8e1a5d19736804d7c21cb603f120d6773edea9
Olena Babko <elena.babko@gmail.com>
2023-11-14 09:21:23 +0200

    subquery and join options

--- HW2/HW2_OlenaBabko.sql
+++ HW2/HW2_OlenaBabko.sql
@@ -17,3 +17,24 @@ JOIN city AS c
 
 
 
+#2 Вивести список міст Аргентини і Австрії. 
+# (таблиці city, country). Відсортувати за алфавітом.
+
+#2 subquery
+SELECT city FROM city
+WHERE country_id in (
+	SELECT country_id FROM country
+    WHERE country.country_id = city.country_id
+    AND country in ("Argentina", "Australia")
+)
+ORDER BY city ASC;
+
+#2  join
+SELECT city FROM city
+JOIN country
+	ON city.country_id = country.country_id
+WHERE country in ("Argentina", "Australia")
+ORDER BY city ASC;
+
+
+
